d48f6ae11862cf57f635f942903496c888fcfe09
Harry Culpan <harry@tesseract.org>
2015-04-18 01:32:38 -0400

    Building interface

---
+++ src/main/java/org/culpan/j6502/BitFlagPanel.java
@@ -0,0 +1,7 @@
+package org.culpan.j6502;
+
+/**
+ * Created by harryculpan on 4/17/15.
+ */
+public class BitFlagPanel {
+}
---
+++ src/main/java/org/culpan/j6502/J6502Instructions.java
@@ -0,0 +1,7 @@
+package org.culpan.j6502;
+
+/**
+ * Created by harryculpan on 4/18/15.
+ */
+public class J6502Instructions {
+}
